18487f61337e5446ae27b05b086ff66f5d59481c
Foram Mehta <mehtaforam1120@gmail.com>
2025-04-26 17:23:06 +0530

    implemented calc avg of moon temperature

--- SolarSystem/obj/Debug/net8.0/SolarSystem.AssemblyInfo.cs
+++ SolarSystem/obj/Debug/net8.0/SolarSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolarSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da1f7292a098d450f0e25410a54d8068dd4571d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolarSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolarSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
